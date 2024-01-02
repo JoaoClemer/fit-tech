a48039de31f7186a2cea938c413e2f15d338c377
João Clemer <joaoclemerlima@gmail.com>
2024-01-02 12:01:28 -0300

    feat : add repository acces for gym actions and add unitofwork pattern

---
+++ src/FitTech.Domain/Repositories/Gym/IGymWriteOnlyRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace FitTech.Domain.Repositories.Gym
+{
+    public interface IGymWriteOnlyRepository
+    {
+        Task CreateGym(Entities.Gym gym);
+    }
+}
---
+++ src/FitTech.Infrastructure/RepositoryAccess/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using FitTech.Domain.Repositories;
+using FitTech.Infrastructure.Context;
+
+namespace FitTech.Infrastructure.RepositoryAccess
+{
+    internal class UnitOfWork : IDisposable, IUnitOfWork
+    {
+        private readonly FitTechContext _context;
+        private bool _disposed;
+
+        public UnitOfWork(FitTechContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if(!_disposed && disposing)
+            {
+                _context.Dispose();
+            }
+
+            _disposed = true;
+        }
+        
+        public async Task Commit()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+       
+    }
+}
