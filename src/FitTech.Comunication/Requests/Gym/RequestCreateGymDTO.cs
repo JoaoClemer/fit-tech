ec3a0f6b98947f3182673b02e021f90c1faee270
João Clemer <joaoclemerlima@gmail.com>
2024-01-03 20:11:06 -0300

    feat: Add request and response DTO for create a gym

---
+++ src/FitTech.Comunication/Requests/Gym/RequestCreateGymDTO.cs
@@ -0,0 +1,12 @@
+﻿using FitTech.Comunication.Requests.Address;
+
+namespace FitTech.Comunication.Requests.Gym
+{
+    public class RequestCreateGymDTO
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public RequestRegisterAddressDTO Address { get; set; }
+    }
+}
