7fbabdd23595f7eb59aa458c9945c68490b7bdbc
João Clemer <joaoclemerlima@gmail.com>
2024-01-26 21:33:22 -0300

    feat: repository class for student

---
+++ src/FitTech.Domain/Repositories/Student/IStudentReadOnlyRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace FitTech.Domain.Repositories.Student
+{
+    public interface IStudentReadOnlyRepository
+    {
+        Task<Entities.Student?> GetStudentByEmail(string email);
+        Task<Entities.Student?> GetStudentByCPF(string cpf);
+        Task<Entities.Student?> GetStudentById(int id);
+        Task<Entities.Student?> GetStudentByRegistrationNumber(int registrationNumber);
+        Task<bool> IsRegisterNumberUnique(int registrationNumber);
+    }
+}
