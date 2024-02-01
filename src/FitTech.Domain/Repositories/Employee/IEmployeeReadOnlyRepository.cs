1001b2280e6959681887c6653dfc5e83593033c8
João Clemer <joaoclemerlima@gmail.com>
2024-02-01 20:23:59 -0300

    feat: add login metodh in employee repository and student repository

--- src/FitTech.Domain/Repositories/Employee/IEmployeeReadOnlyRepository.cs
+++ src/FitTech.Domain/Repositories/Employee/IEmployeeReadOnlyRepository.cs
@@ -5,5 +5,6 @@
         Task<Entities.Employee?> GetEmployeeByEmail(string email);
         Task<Entities.Employee?> GetEmployeeByCPF(string cpf);
         Task<Entities.Employee?> GetEmployeeById(int id);
+        Task<Entities.Employee?> Login(string email, string password);
     }
 }
