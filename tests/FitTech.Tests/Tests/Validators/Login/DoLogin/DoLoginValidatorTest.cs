7ecdb61692e85b0159af7e377b6a061beb0a3183
João Clemer <joaoclemerlima@gmail.com>
2024-02-05 20:15:57 -0300

    test: do login validator tests

---
+++ tests/FitTech.Tests/Tests/Validators/Login/DoLogin/DoLoginValidatorTest.cs
@@ -0,0 +1,104 @@
+﻿using FitTech.Application.UseCases.Gym.Create;
+using FitTech.Application.UseCases.Login.DoLogin;
+using FitTech.Application.UseCases.Student.Create;
+using FitTech.Exceptions;
+using FitTech.Tests.Utils.Requests;
+using FluentAssertions;
+using Xunit;
+
+namespace FitTech.Tests.Tests.Validators.Login.DoLogin
+{
+    public class DoLoginValidatorTest
+    {
+        [Fact]
+        public void Valid_Success()
+        {
+            var validator = new DoLoginValidator();
+
+            var request = RequestDoLoginBuilder.Build();
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeTrue();
+
+        }
+
+        [Fact]
+        public void Valid_Error_Empty_Email()
+        {
+            var validator = new DoLoginValidator();
+
+            var request = RequestDoLoginBuilder.Build();
+            request.EmailAddress = string.Empty;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.EMPTY_EMAIL));
+
+        }
+
+        [Fact]
+        public void Valid_Error_Empty_Password()
+        {
+            var validator = new DoLoginValidator();
+
+            var request = RequestDoLoginBuilder.Build();
+            request.Password = string.Empty;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.EMPTY_PASSWORD));
+
+        }
+
+        [Fact]
+        public void Valid_Error_Empty_User_Type()
+        {
+            var validator = new DoLoginValidator();
+
+            var request = RequestDoLoginBuilder.Build();
+            request.UserType = 0;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.EMPTY_USER_TYPE));
+
+        }
+
+        public void Valid_Error_Invalid_Email()
+        {
+            var validator = new DoLoginValidator();
+
+            var request = RequestDoLoginBuilder.Build();
+            request.EmailAddress = "invalid.com";
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.INVALID_EMAIL));
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void Valid_Error_Invalid_Password(int passwordLength)
+        {
+            var validator = new DoLoginValidator();
+
+            var request = RequestDoLoginBuilder.Build(passwordLength);
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.INVALID_PASSWORD));
+
+        }
+
+    }
+}
---
+++ tests/FitTech.Tests/Utils/Requests/RequestDoLoginBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using FitTech.Comunication.Enum;
+using FitTech.Comunication.Requests.Login;
+
+namespace FitTech.Tests.Utils.Requests
+{
+    public static class RequestDoLoginBuilder
+    {
+        public static RequestDoLoginDTO Build(int passwordLenght = 6)
+        {
+            return new Faker<RequestDoLoginDTO>()
+                .RuleFor(l => l.EmailAddress, f => f.Internet.Email())
+                .RuleFor(l => l.Password, f => f.Internet.Password(passwordLenght))
+                .RuleFor(l => l.UserType, f => f.Random.Enum<UserTypeDTO>());
+        }
+    }
+}
